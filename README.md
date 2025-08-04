9e9acb89d0ecedace1ae01083eedb27ec73f80ee
Saller Yero Rezende <16264@ait.nsw.edu.au>
2025-08-04 18:18:55 +1000

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# ProGear-Hub
+# classwork
+website project for foundation of web development 2025
---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include "includes/head.php"; ?>
+
+<body>
+    <?php include "includes/pageheader.php" ?>
+    <div class="home-banner">
+        <div class="banner-text">
+            <h2>SALE</h2>
+            <p>Get massive discounts in our mid-year sale</p>
+            <a href="sale.html">Shop</a>
+        </div>
+    </div>
+    <main class="container">
+        <div class="card">
+            <img src="products/pexels-cottonbro-3927387.jpg" alt="sports watch with accessories">
+            <div class="card-content">
+                <h4 class="card-title">Product 1</h4>
+                <p class="price">199</p>
+                <a href="#" class="card-button">
+                    Add to cart
+                </a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="products/pexels-kowalievska-1040424.jpg" alt="a woman wearing a black hoodie">
+            <div class="card-content">
+                <h4 class="card-title">Product 2</h4>
+                <p class="price">299</p>
+                <a href="#" class="card-button">
+                    Add to cart
+                </a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="products/pexels-mastercowley-1199607.jpg" alt="women doing push ups on the beach">
+            <div class="card-content">
+                <h4 class="card-title">Product 3</h4>
+                <p class="price">99</p>
+                <a href="#" class="card-button">
+                    Add to cart
+                </a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="products/pexels-ollivves-1103828.jpg" alt="a guy standing next to tennis net">
+            <div class="card-content">
+                <h4 class="card-title">Product 4</h4>
+                <p class="price">199</p>
+                <a href="#" class="card-button">
+                    Add to cart
+                </a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="products/pexels-olly-3768901.jpg" alt="a guy doing push ups in a park">
+            <div class="card-content">
+                <h4 class="card-title">Product 5</h4>
+                <p class="price">199</p>
+                <a href="#" class="card-button">
+                    Add to cart
+                </a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="products/pexels-olly-869243.jpg" alt="women in fitness gym">
+            <div class="card-content">
+                <h4 class="card-title">Product 6</h4>
+                <p class="price">199</p>
+                <a href="#" class="card-button">
+                    Add to cart
+                </a>
+            </div>
+        </div>
+        <div class="bottom-banner">
+            <div class="banner-text">
+                <h2>Clearance Sale</h2>
+                <p>Get massive discounts</p>
+                <a href="clearance.html">Shop Clearance</a>
+            </div>
+        </div>
+    </main>
+    <?php include "includes/footer.php"; ?>
+</body>
+
+</html>
